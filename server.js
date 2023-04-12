a6196fc7b78c55eb8fb8aebee3b46416917c086e
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-04-13 00:31:58 +0530

    1st

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const colors = require('colors')
+const moragan = require('morgan')
+const dotenv = require('dotenv')
+
+//rest object
+const app = express()
+
+//middlewares
+app.use(express.json())
+app.use(moragan('dev'))
+
+//routes
+app.get('/', (req, res) => {
+    res.status(200).send({
+        message: "server running",
+    });
+});
+
+//port
+const port = process.env.PORT || 8080
+//listen port
+app.listen(port, () =>{
+    console.log(
+        `Server Running in ${process.env.NODE_MODE} Mode on port ${process.env.PORT}`
+            .bgCyan.white
+        )
+})
