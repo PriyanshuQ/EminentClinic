c42813dd971f5076b632a6cf48507472fd5c31a2
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-06 15:33:21 +0530

    commit

--- server.js
+++ server.js
@@ -7,11 +7,19 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute")
 const adminRoute = require('./routes/adminRoute')
 const doctorsRoute = require('./routes/doctorsRoute')
+const path = require("path");
 
 app.use("/api/user", userRoute)
 app.use("/api/admin", adminRoute)
 app.use("/api/doctor", doctorsRoute)
 const port = process.env.PORT || 5000;
 
+if (process.env.NODE_ENV === "production") {
+    app.use("/", express.static("client/build"));
+  
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+    });
+  }
 
 app.listen(port, () => console.log(`Node server started at port ${port}`))
