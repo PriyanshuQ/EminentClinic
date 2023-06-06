7269dc1a2c924a8b50a5b6beb068a2b34bf9e230
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-06 16:04:49 +0530

    commit

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
-const express = require('express')
-const app = express()
-require('dotenv').config()
-const dbConfig = require("./config/dbConfig")
+const express = require("express");
+const app = express();
+require("dotenv").config();
+const dbConfig = require("./config/dbConfig");
 app.use(express.json());
 
-const userRoute = require("./routes/userRoute")
-const adminRoute = require('./routes/adminRoute')
-const doctorsRoute = require('./routes/doctorsRoute')
+const userRoute = require("./routes/userRoute");
+const adminRoute = require("./routes/adminRoute");
+const doctorsRoute = require("./routes/doctorsRoute");
 const path = require("path");
 
-app.use("/api/user", userRoute)
-app.use("/api/admin", adminRoute)
-app.use("/api/doctor", doctorsRoute)
+app.use("/api/user", userRoute);
+app.use("/api/admin", adminRoute);
+app.use("/api/doctor", doctorsRoute);
 const port = process.env.PORT || 5000;
 
 if (process.env.NODE_ENV === "production") {
-    app.use("/", express.static("client/build"));
-  
-    app.get("*", (req, res) => {
-      res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-    });
-  }
+  app.use("/", express.static("client/build"));
 
-app.listen(port, () => console.log(`Node server started at port ${port}`))
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
+  });
+}
+
+app.listen(port, () => console.log(`Node server started at port ${port}`));
