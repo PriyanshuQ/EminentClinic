d5df27548a4ef61f7686bcb8ef6cf2c450b57d37
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-11 22:44:58 +0530

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ app.use(express.static(path.join(__dirname, "./client/build")));
 
 //rest api
 app.use("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../client/build/index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
 });
 
 app.use("/api/user", userRoute);
