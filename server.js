89128c5cae77315548c1b329126bd11f9556c4d9
PriyanshuQ <80645068+PriyanshuQ@users.noreply.github.com>
2023-06-11 22:07:00 +0530

    commit

--- server.js
+++ server.js
@@ -7,19 +7,22 @@ app.use(express.json());
 const userRoute = require("./routes/userRoute");
 const adminRoute = require("./routes/adminRoute");
 const doctorsRoute = require("./routes/doctorsRoute");
-// const path = require("path");
+const path = require("path");
+
+app.use(express.static(path.join(__dirname, './client/build')))
+
+//rest api
+app.user("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "./client/build/index.html"));
+  });
 
 app.use("/api/user", userRoute);
 app.use("/api/admin", adminRoute);
 app.use("/api/doctor", doctorsRoute);
 const port = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV === "production") {
-  app.use("/", express.static("client/build"));
-
-  app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client/build/index.html"));
-  });
-}
+// if (process.env.NODE_ENV === "production") {
+//   app.use("/", express.static("client/build"));
+// }
 
 app.listen(port, () => console.log(`Node server started at port ${port}`));
